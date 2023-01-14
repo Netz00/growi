fdc83656a903b308461072fcb1be3f990bf5bda1
Netz <bozo2000d@gmail.com>
2023-01-14 13:44:20 +0100

    Refactoring - extracting Base component to pages
    Enable using different styling for pages, withouthaving to add style config inside JSON page content

--- src/pages/brands.tsx
+++ src/pages/brands.tsx
@@ -1,8 +1,75 @@
 import fs from 'fs';
 
-import Base from '../components/templates/Base';
+import Link from 'next/link';
 
-const Brands = (content: any) => <Base {...content} />;
+import { Button } from '../components/button/Button';
+import { Meta } from '../components/layout/Meta';
+import { Banner } from '../components/templates/Banner';
+import { Footer } from '../components/templates/Footer';
+import { Hero } from '../components/templates/Hero';
+import { VerticalFeatures } from '../components/templates/VerticalFeatures';
+import { AppConfig } from '../utils/AppConfig';
+
+const Brands = (props: any) => (
+	<div className="antialiased text-gray-600">
+		<Meta
+			title={props.siteTitle ?? AppConfig.title}
+			description={props.siteDescription ?? AppConfig.description}
+		/>
+		<Hero
+			navBarItems={
+				<>
+					{props.hero.navBarLinks?.map((item: any) =>
+						item.active ? (
+							<li
+								key={item.key}
+								className="underline underline-offset-2 decoration-primary-500 text-primary-500 pointer-events-none"
+							>
+								{item.text}
+							</li>
+						) : (
+							<li key={item.key}>
+								<Link href={item.link}>{item.text}</Link>
+							</li>
+						)
+					)}
+				</>
+			}
+			title={
+				<>
+					{props.hero.title.part1}
+					<span className="text-primary-500">
+						{props.hero.title.part2}
+					</span>
+				</>
+			}
+			description={props.hero.description}
+			buttons={
+				<>
+					{props.hero.buttons.map((button: any) => (
+						<Link key={button.key} href={button.link}>
+							<Button xl>{button.text}</Button>
+						</Link>
+					))}
+				</>
+			}
+		/>
+
+		{props.sections.map((section: any) => (
+			<VerticalFeatures {...section} key={section.key} />
+		))}
+
+		<Banner
+			{...props.banner}
+			button={
+				<Link href={props.banner.button.link}>
+					<Button>{props.banner.button.text}</Button>
+				</Link>
+			}
+		/>
+		<Footer {...props.footer} />
+	</div>
+);
 
 export async function getStaticProps() {
 	/**
